817514ac2b1fc6c8fc2234927d3fa8ecb761e362
jimmymcperson <hao_jonathanh@hotmail.com>
2016-05-17 12:55:53 -0400

    Added a reference to driver class from unit test.

---
+++ COMP123-S2016-Lesson2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: Jonathan Hao
+ * Date: May 17, 2016
+ * Date Modified: May 17, 2016
+ * Description: Advanced Methods Demo for Lesson 2
+ * Version: 0.0.2 - Added a reference to driver class from unit test.
+ */
+namespace COMP123_S2016_Lesson2
+{
+    /**
+     * Main driver class for Lesson 2
+     * 
+     * @class Program
+     */
+    class Program
+    {
+        /**
+         * Main method for our drive class
+         * 
+         * @constructor Main
+         * @param {string[]} args
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
