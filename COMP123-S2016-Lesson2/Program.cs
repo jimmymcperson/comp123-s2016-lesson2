6250b9d6ada782fe39438b8219e580c86c291679
jimmymcperson <hao_jonathanh@hotmail.com>
2016-05-17 13:27:55 -0400

    Added addXandY method

--- COMP123-S2016-Lesson2/Program.cs
+++ COMP123-S2016-Lesson2/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
  * Date: May 17, 2016
  * Date Modified: May 17, 2016
  * Description: Advanced Methods Demo for Lesson 2
- * Version: 0.0.2 - Added a reference to driver class from unit test.
+ * Version: 0.0.3 - Added addXandY method
  */
 namespace COMP123_S2016_Lesson2
 {
@@ -18,7 +18,7 @@ namespace COMP123_S2016_Lesson2
      * 
      * @class Program
      */
-    class Program
+    public class Program
     {
         /**
          * Main method for our drive class
@@ -28,6 +28,24 @@ namespace COMP123_S2016_Lesson2
          */
         static void Main(string[] args)
         {
+            int x = 50;
+            int y = 40;
+            int result = 0;
+
+            Console.WriteLine(addXandY(x, y));
+        }
+        /**
+         * A simple method to add two values
+         * 
+         * @method addXandY
+         * @param {int} firstNumber
+         * @param {int} secondNumber
+         */
+        public static int addXandY(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+            result = firstNumber + secondNumber;
+            return result;
         }
     }
 }
---
+++ UnitTestForLesson2/UnitTestForLesson2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_Lesson2;
+
+namespace UnitTestForLesson2
+{
+    [TestClass]
+    public class UnitTestForLesson2
+    {
+        [TestMethod]
+        public void addXandYTestMethod()
+        {
+            // Arrange
+            int x = 50;
+            int y = 40;
+            int expectedResult = 90;
+            
+            // Act
+            int result = Program.addXandY(x, y);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
